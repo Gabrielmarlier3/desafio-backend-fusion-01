a02d1711d8b26e985d2497049688b6d8fb9092a4
Gabrielmarlier3 <gabrielmarliere2005@gmail.com>
2024-10-07 08:53:07 -0300

    eslintrc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
-import { AuthService } from "./auth.service";
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private service: AuthService){
-    }
+  constructor(private service: AuthService) {}
 
-    @ApiOperation({ summary: 'Get the JWT token to use for the other endpoints' })
-    @Get()
-    async getToken(){
-        return this.service.generateToken();
-    }
+  @ApiOperation({ summary: 'Get the JWT token to use for the other endpoints' })
+  @Get()
+  async getToken() {
+    return this.service.generateToken();
+  }
 }
